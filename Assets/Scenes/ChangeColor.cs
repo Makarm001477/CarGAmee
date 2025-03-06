bbbee7ba7f28f0aab9bf5fe0b41f74c8fd62fb9a
makarm001477 <makarm.mek1000@gmail.com>
2025-03-07 03:17:17 +0700

    Finish

---
+++ Assets/Scenes/ChangeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorLoop : MonoBehaviour
+{
+    public float changeInterval = 1f; // เปลี่ยนสีทุกๆ 1 วินาที
+
+    void Start()
+    {
+        InvokeRepeating("ChangeCubeColor", 0f, changeInterval);
+    }
+
+    void ChangeCubeColor()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.color = new Color(Random.value, Random.value, Random.value);
+        }
+    }
+}
---
+++ Assets/Scenes/ResetGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // ต้องใช้ SceneManager
+
+public class ResetGame : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) // ตรวจสอบว่ากด R หรือไม่
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // โหลดฉากปัจจุบันใหม่
+        }
+    }
+}
